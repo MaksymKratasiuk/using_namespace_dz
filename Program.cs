ce3fbc212b61a50617870271ae7f952e357e8e00
MaksymKratasiuk <makshm1307@gmail.com>
2023-04-29 16:45:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace using_namespace_dz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-2
+            TicTacToeS ticTacToeS = new TicTacToeS();
+            ticTacToeS.Play();
+            TicTacToeM ticTacToeM = new TicTacToeM();
+            ticTacToeM.Play();
+            //3-4
+            Test3_4.num3_4();
+        }
+    }
+}
